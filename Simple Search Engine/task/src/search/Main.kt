5f633b4a8bc8219a69fa64af3b4a30c1317d2cdc
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-02-25 16:51:14 +0200

    Thu., Feb. 25th 2021 16:51

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,15 +1,52 @@
 package search
 
-fun main() {
-    val line = readLine()!!.split(" ")
-    val word = readLine()!!
 
-    for ((i, w) in line.withIndex()) {
-        if (w == word) {
-            println(i + 1)
-            return
+val data = arrayListOf<String>()
+
+fun getData() {
+    println("Enter the number of people:")
+    val numPeople = readLine()!!.toInt()
+
+    println("Enter all people:")
+    repeat(numPeople) {
+        print("> ")
+        data.add(readLine()!!)
+    }
+    println()
+}
+
+fun getQueries() {
+    println("Enter the number of search queries:")
+    val numQueries = readLine()!!.toInt()
+    println()
+
+    repeat(numQueries) {
+        println("Enter data to search people:")
+        val query = readLine()!!
+
+        println("\nFound people:")
+        val fp = findPeople(query)
+        if (fp.isEmpty())
+            println("No matching people found.")
+        else {
+            for (p in fp)
+                println(p)
         }
     }
+    println()
+}
 
-    println("Not Found")
+fun findPeople(query: String): ArrayList<String> {
+    val result = arrayListOf<String>()
+    for (line in data) {
+        if (line.toLowerCase().contains(query.toLowerCase())) {
+            result.add(line)
+        }
+    }
+    return result
+}
+
+fun main() {
+    getData()
+    getQueries()
 }
